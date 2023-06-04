8930c9c9fadfa2ab84f766e601a5c8e647bb7efc
LaloSbrnk <135392331+LaloSbrnk@users.noreply.github.com>
2023-06-04 19:20:09 -0300

    Actualizacion de Omnibus.
    Intento de implementar la ocupacion o desocupacion de asientos utilizando la clase Asiento.

--- src/ViajeImprovisado/Omnibus.java
+++ src/ViajeImprovisado/Omnibus.java
@@ -1,13 +1,17 @@
 package ViajeImprovisado;
 
+import java.util.ArrayList;
+
 public class Omnibus {
 	private String nombre, empresa;
 	private int asientos;
+	private Asiento[] arrayAsientos;
 	
 	public Omnibus(String nombre, int asientos) {
 		super();
 		this.nombre = nombre;
 		this.asientos = asientos;
+		this.arrayAsientos = new Asiento[asientos];
 	}
 	
 	public String getEmpresa() {
@@ -26,4 +30,25 @@ public class Omnibus {
 		return "Omnibus [Nombre= " + nombre +
 									"\nAsientos= " + asientos + "]\n";
 	}
+
+	public void ocuparAsiento(int numeroDeAsiento, Cliente nuevoPasajero){
+		if(arrayAsientos[numeroDeAsiento]==null){
+			arrayAsientos[numeroDeAsiento]= new Asiento(nuevoPasajero, numeroDeAsiento);
+		}
+		else{
+			if (!(arrayAsientos[numeroDeAsiento].estaOcupado())){
+				arrayAsientos[numeroDeAsiento].setPasajero(nuevoPasajero);
+			}
+		}
+	}
+	public void desocuparAsiento(int numeroDeAsiento){
+		if(arrayAsientos[numeroDeAsiento]==null){
+			//no habia asiento ocupado
+		}else{
+			if (!(arrayAsientos[numeroDeAsiento].estaOcupado())){
+				arrayAsientos[numeroDeAsiento].desocuparAsiento();
+			}
+		}
+	}
+
 }
