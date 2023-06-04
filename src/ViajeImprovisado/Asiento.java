22b1ae4202cf66ec5e9e2ee553dc0b69e2344ca8
LaloSbrnk <135392331+LaloSbrnk@users.noreply.github.com>
2023-06-04 17:22:59 -0300

    Update Asiento.java
    Posible implementacion de la clase Asiento

--- src/ViajeImprovisado/Asiento.java
+++ src/ViajeImprovisado/Asiento.java
@@ -1,3 +1,51 @@
 package ViajeImprovisado;
 public class Asiento {
+
+    private Cliente pasajero;
+
+    private int numeroDeAsiento;
+
+    //cargar asiento ocupado directamente
+    public Asiento(Cliente pasajero, int numeroDeAsiento){
+        this.pasajero = pasajero;
+        if (numeroDeAsiento >= 0){
+            this.numeroDeAsiento = numeroDeAsiento;
+        }
+    }
+
+    //crear asiento sin pasajero para luego ser ocupado
+    public Asiento(int numeroDeAsiento){
+        this.pasajero = null;
+        this.numeroDeAsiento = numeroDeAsiento;
+    }
+
+    public Cliente getPasajero() {
+        return this.pasajero;
+    }
+
+    public void setPasajero(Cliente pasajero) {
+        if (!estaOcupado()) {
+            this.pasajero = pasajero;
+        } else {
+            //System.out.println("El asiento ya está ocupado por otro pasajero.");
+        }
+    }
+    public int getNumeroDeAsiento() {
+        return this.numeroDeAsiento;
+    }
+
+    public void setNumeroDeAsiento(int numeroDeAsiento) {
+        if (numeroDeAsiento >= 0){
+            this.numeroDeAsiento = numeroDeAsiento;
+        }
+    }
+    
+    public boolean estaOcupado(){
+        return pasajero != null;
+    }
+    
+    public void desocuparAsiento() {
+        pasajero = null;
+    }
+
 }
