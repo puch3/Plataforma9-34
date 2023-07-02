7f55f28f6a4ec292fb27a5221347273c2a7e4b98
LaloSbrnk <135392331+LaloSbrnk@users.noreply.github.com>
2023-07-02 03:50:40 -0300

    Implementacion del metodo mostrarAsientos y seleccionarAsientos

--- src/ViajeImprovisado/Omnibus.java
+++ src/ViajeImprovisado/Omnibus.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Omnibus {
 	private String nombre;
 	private String empresa;
 	private ArrayList<Asiento> asientos;
+	private int numColumnas;
+	private int numFilas;
 	
 	public Omnibus(String nombre, int capacidad) {
 		super();
@@ -16,6 +19,8 @@ public class Omnibus {
 		}
 
 		this.asientos = aux;
+		this.numColumnas = 4;//lo usual en omnibus
+		this.numFilas =(int)Math.ceil(capacidad / numColumnas);
 	}
 
 	public void addAsiento(Asiento a){
@@ -41,5 +46,43 @@ public class Omnibus {
 	public String toString() {
 		return "Omnibus [Nombre= " + nombre +
 									"\nAsientos= " + asientos + "]\n";
+	}
+	public void mostrarAsientos() {
+		System.out.println("Asientos del omnibus:");
+		System.out.println();
+
+		for (int i = 0; i < asientos.size(); i++) {
+			if (i % numColumnas == 0 && i != 0) {
+				System.out.println();
+			}
+
+			if (i % numColumnas == numColumnas / 2) {
+				System.out.print("    ");
+			}
+
+			if (i + 1 < 10) {
+				System.out.print(" ");
+			}
+
+			System.out.print((asientos.get(i).getNumeroDeAsiento()) + ": ");
+			if ((asientos.get(i).estaOcupado())) {
+				System.out.print("X  ");
+			} else {
+				System.out.print("O  ");
+			}
+		}
+		System.out.println();
+	}
+	public ArrayList<Asiento> seleccionarAsientos() {
+		ArrayList<Asiento> asientosSeleccionados = new ArrayList<Asiento>();
+		Scanner scA = new Scanner(System.in);
+		System.out.print("Seleccione el/los asiento/s deseado/s (numero-numero)");
+		String in = scA.nextLine();
+		String[] numerosAsientos = in.split("-");
+		for (String numero : numerosAsientos) {
+			int numeroAsiento = Integer.parseInt(numero);
+			asientosSeleccionados.add(asientos.get(numeroAsiento - 1));
+		}
+		return asientosSeleccionados;
 	}
 }
