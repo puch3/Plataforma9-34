6b989a85d343d8a529237eceec7f0a76b1ed72bf
LaloSbrnk <135392331+LaloSbrnk@users.noreply.github.com>
2023-07-02 03:34:09 -0300

    Correcion CriterioHoraSalida

--- src/ViajeImprovisado/CriterioHoraSalida.java
+++ src/ViajeImprovisado/CriterioHoraSalida.java
@@ -1,6 +1,8 @@
+import java.time.LocalTime;
+
 public class CriterioHoraSalida implements Criterio{
-    private LocalDate horaSalida1, horaSalida2;
-    public CriterioHoraSalida(LocalDate horaSalida1, LocalDate horaSalida2){
+    private LocalTime horaSalida1, horaSalida2;
+    public CriterioHoraSalida(LocalTime horaSalida1, LocalTime horaSalida2){
         this.horaSalida1 = horaSalida1;
         this.horaSalida2 = horaSalida2;
     }
@@ -9,16 +11,16 @@ public class CriterioHoraSalida implements Criterio{
         (horaSalida1.isBefore(LocalTime.parse(v.getHoraSalida())) && 
         horaSalida2.isAfter(LocalTime.parse(v.getHoraSalida()))));
     }
-    public LocalDate getHoraSalida1(){
+    public LocalTime getHoraSalida1(){
         return this.horaSalida1;
     }
-    public LocalDate getHoraSalida2(){
+    public LocalTime getHoraSalida2(){
         return this.horaSalida2;
     }
-    public void setHoraSalida1(LocalDate hs1){
+    public void setHoraSalida1(LocalTime hs1){
         this.horaSalida1 = hs1;
     }
-    public void setHoraSalida2(LocalDate hs2){
+    public void setHoraSalida2(LocalTime hs2){
         this.horaSalida2 = hs2;
     }
 }
